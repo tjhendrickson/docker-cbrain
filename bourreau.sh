d30ac7bb55480a1771ab7b27a0c60c527983acf9
Ludovic Claude <ludovic.claude54@gmail.com>
2016-06-29 17:35:42 +0200

    Generate ssh host keys

--- bourreau.sh
+++ bourreau.sh
@@ -19,6 +19,41 @@ function install_plugins_bourreau {
     rake cbrain:plugins:install:plugins || die "Cannot install plugins"
 }
 
+# Generate Host keys, if required
+function generate_ssh_host_keys {
+    local RSA_KEY=/etc/ssh/ssh_host_rsa_key
+    local DSA_KEY=/etc/ssh/ssh_host_dsa_key
+    local KEYGEN=/usr/bin/ssh-keygen
+
+    if [ ! -s $RSA_KEY ]; then
+        echo -n "Generating SSH2 RSA host key: "
+        rm -f $RSA_KEY
+        if test ! -f $RSA_KEY && $KEYGEN -q -t rsa -f $RSA_KEY -C '' -N '' >&/dev/null; then
+            chmod 600 $RSA_KEY
+            chmod 644 $RSA_KEY.pub
+            if [ -x /sbin/restorecon ]; then
+                /sbin/restorecon $RSA_KEY.pub
+            fi
+        else
+            die "RSA key generation failed"
+        fi
+    fi
+
+    if [ ! -s $DSA_KEY ]; then
+        echo -n "Generating SSH2 DSA host key: "
+        rm -f $DSA_KEY
+        if test ! -f $DSA_KEY && $KEYGEN -q -t dsa -f $DSA_KEY -C '' -N '' >&/dev/null; then
+            chmod 600 $DSA_KEY
+            chmod 644 $DSA_KEY.pub
+            if [ -x /sbin/restorecon ]; then
+                /sbin/restorecon $DSA_KEY.pub
+            fi
+        else
+            die $"DSA key generation failed"
+        fi
+    fi
+}
+
 ###############
 # Main script #
 ###############
@@ -45,10 +80,7 @@ then
         chown cbrain:cbrain ${volume}
     done
     su cbrain "$0" "$@"
-    # Generate Host keys, if required
-    if ! ls /etc/ssh/ssh_host_* 1> /dev/null 2>&1; then
-      ssh-keygen -A
-    fi
+    generate_ssh_host_keys
     echo "Starting bourreau"
     exec /usr/sbin/sshd -D 
 fi
